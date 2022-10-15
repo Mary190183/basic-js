1775a97ff97a206e6d92105544941ee585bb9211
Mariya Petrovskaya <pma190183@yandex.ru>
2022-10-15 17:50:35 +0300

    feat: transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,63 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+		throw new Error('input argument must be an array')
+	}
+
+	let transformedArray = []
+
+	let doubleNext = false
+	let discardNext = false
+	let prevHasBeenDiscarded = false
+
+	for (let x of arr) {
+		switch (x) {
+			case '--discard-next':
+				discardNext = true
+				break;
+			case '--double-next':
+				doubleNext = true
+				break;
+			case '--discard-prev':
+				if (transformedArray.length > 0 && !prevHasBeenDiscarded) {
+					transformedArray.pop()	
+					prevHasBeenDiscarded = false
+				}
+				break;
+			case '--double-prev':
+				if (transformedArray.length > 0 && !prevHasBeenDiscarded) {
+					transformedArray.push(transformedArray[transformedArray.length - 1])
+					prevHasBeenDiscarded = false
+				}
+				break;
+			default:
+				if (discardNext) {
+					discardNext = false
+					prevHasBeenDiscarded = true
+					break;
+				}
+
+				prevHasBeenDiscarded = false;
+				transformedArray.push(x)
+				if (doubleNext) {
+					doubleNext = false
+					transformedArray.push(x)
+				}
+		}
+	}
+
+	return transformedArray
 }
+
+
+	
+
+
+  // throw new NotImplementedError('Not implemented');
+  // remove line with error and write your code here
+
 
 module.exports = {
   transform
