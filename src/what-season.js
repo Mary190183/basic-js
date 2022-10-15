c12359f8cc19b009992c3deaf2923ce836e3a404
Mariya Petrovskaya <pma190183@yandex.ru>
2022-10-15 16:08:29 +0300

    feat: season

--- src/what-season.js
+++ src/what-season.js
@@ -12,12 +12,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
  
-function getSeason(/*date*/) {
-  
+function getSeason(date) {
+  if (!date) return "Unable to determine the time of year!";
+  if (!(date instanceof Date)) throw new Error("Invalid date!");
+
+  if (`${Object.entries(date)}` != `${Object.entries(new Date(date))}`)
+  throw new Error("Invalid date!");
+
+const month = date.getUTCMonth();
+switch (month) {
+  case 2:
+  case 3:
+  case 4:
+    return "spring";
+  case 5:
+  case 6:
+  case 7:
+    return "summer";
+  case 8:
+  case 9:
+  case 10:
+    return "autumn";
+  case 11:
+  case 0:
+  case 1:
+    return "winter";
+}
+
+
 
 
  
-  throw new NotImplementedError('Not implemented');
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
