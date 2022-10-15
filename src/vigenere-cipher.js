99e3a7785f0844f4c36fbc646cd43b72dfc8807a
Mariya Petrovskaya <pma190183@yandex.ru>
2022-10-15 16:11:48 +0300

    feat: vigenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,20 +19,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
- 
-  
-  
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class VigenereCipheringMachine {
+  constructor(isDirect = true) {
+    Object.assign(this, { isDirect });
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  
+  #ALPH = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  #tabulaRecta = [...this.#ALPH].reduce((p, c, i) => {
+    return [...p, this.#ALPH.slice(i) + this.#ALPH.slice(0, i)];
+  }, []);
+  #getIndexes(...strings) {
+    return strings.map(str => [...str].map(char => this.#ALPH.indexOf(char)));
+  }
+  #checkArguments(a, b) {
+    if (!a || !b) throw new Error("Incorrect arguments!");
+  }
+  #reverseString(str) {
+    return [...str].reverse().join("");
+  }
+  encrypt(message, key) {
+    this.#checkArguments(...arguments);
+    message = message.toUpperCase();
+    key = key.padEnd(message.length, key).toUpperCase();
+    const [mIndexes, kIndexes] = this.#getIndexes(message, key);
+    let result = "";
+    for (let i = 0, j = 0; i < message.length; i++) {
+      result += this.#ALPH.includes(message[i])
+        ? this.#tabulaRecta[mIndexes[i]][kIndexes[j++]]
+        : message[i];
+    }
+    return this.isDirect ? result : this.#reverseString(result);
+  }
+  decrypt(message, key) {
+    this.#checkArguments(...arguments);
+    message = message.toUpperCase();
+    key = key.padEnd(message.length, key).toUpperCase();
+    const [mIndexes, kIndexes] = this.#getIndexes(message, key);
+    const { length } = this.#ALPH;
+    let result = "";
+    for (let i = 0, j = 0; i < message.length; i++) {
+      result += this.#ALPH.includes(message[i])
+        ? this.#tabulaRecta[mIndexes[i]][(length - kIndexes[j++]) % length]
+        : message[i];
+    }
+    return this.isDirect ? result : this.#reverseString(result);
+  }
 }
-}
+
 module.exports = {
   VigenereCipheringMachine
 };
